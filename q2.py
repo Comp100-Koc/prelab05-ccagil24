23eef23b4850703f10d4deacc5a404d444db5a6b
ccagil24 <ccagil24@ku.edu.tr>
2026-04-03 03:09:21 +0300

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,5 +1,28 @@
+
 def remove_adjacent_duplicates(s):
-    '''
-    Given a string remove all the adjacent duplicate characters and return the string
-    '''
-    pass
+    while adjacent_duplicates_exist(s):
+        result = ""
+        for i in range(len(s)):
+            
+            # if the current character is equal to the next character
+            if i < len(s)-1 and s[i] == s[i+1]:
+                
+                # add the rest of the string to the result
+                result += s[i+2:]
+                
+                # break the loop
+                break
+            else:
+                # add the current character to the result
+                result += s[i]
+        
+        # update the string
+        s = result
+        
+    return s
+
+def adjacent_duplicates_exist(s):
+    for i in range(len(s)-1):
+        if s[i] == s[i+1]:
+            return True
+    return False
